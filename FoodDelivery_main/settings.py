374eb07875a2ced9dc5cb2c4c9cdffaf50f45f0e
Aryavardhan Jain <jainaryan418@gmail.com>
2023-11-02 10:04:05 +0530

    Error

--- FoodDelivery_main/settings.py
+++ FoodDelivery_main/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'accounts',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -84,6 +85,7 @@ DATABASES = {
     }
 }
 
+AUTH_USER_MODEL = 'accounts.User'
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
@@ -124,6 +126,9 @@ STATICFILES_DIRS = [
     'FoodDelivery_main/static'
 ]
 STATIC_ROOT = BASE_DIR / 'static'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
